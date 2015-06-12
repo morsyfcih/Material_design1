57ce8150ca1818522cec259568576a845fa0e648
Mohamed Morsy <morsyfcih@gmail.com>
2015-06-12 04:17:17 +0200

    Initial commit

---
+++ app/src/main/java/design/material/qatra/model/NavDrawerItem.java
@@ -0,0 +1,30 @@
+package design.material.qatra.model;
+
+/**
+ * Created by Morsy on 08/06/2015.
+ */
+public class NavDrawerItem {
+    private boolean showNotify;
+    private String title;
+    public NavDrawerItem() {
+        }
+    public NavDrawerItem(boolean showNotify, String title) {
+        this.showNotify = showNotify;
+        this.title = title;
+        }
+
+    public boolean isShowNotify() {
+        return showNotify;
+        }
+    public void setShowNotify(boolean showNotify) {
+        this.showNotify = showNotify;
+        }
+
+            public String getTitle() {
+        return title;
+        }
+
+            public void setTitle(String title) {
+        this.title = title;
+        }
+}
